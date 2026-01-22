4a3446b0c724b3f72e03c1d8d624be155baf0b8b
JulienPatron <julien.patron@outlook.fr>
2026-01-22 20:30:20 +0100

    main

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ st.set_page_config(layout="wide", page_title="Julien Patron - Portfolio")
 # 1. Portfolio (Il est à la racine, donc on met juste le nom du fichier)
 portfolio_page = st.Page(
     "01_Portfolio_Optimizer.py", 
-    title="Portfolio Optimizer" 
+    title="Portfolio Optimizer",
     default=True
 )
 
 # 2. Movie System (Il est dans le dossier pages, donc on met "pages/...")
 movie_page = st.Page(
     "pages/03_Movie_Recommendation_System.py", 
-    title="Movie Recommendation System"
+    title="Movie Recommendation System",
 )
 
 # --- CRÉATION DE LA NAVIGATION (GROUPÉE) ---
