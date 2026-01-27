f1003948b96d6ef45bdb67c38dfcb96a64de73e5
JulienPatron <julien.patron@outlook.fr>
2026-01-27 16:17:55 +0100

    main

--- main.py
+++ main.py
@@ -5,24 +5,33 @@ st.set_page_config(layout="wide", page_title="Julien Patron - Portfolio")
 
 # --- DÉFINITION DES PAGES ---
 
-# 1. Portfolio (Il est à la racine, donc on met juste le nom du fichier)
+# 1. Portfolio (Finance)
 portfolio_page = st.Page(
     "01_Portfolio_Optimizer.py", 
     title="Portfolio Optimizer",
+    icon="💰",
     default=True
 )
 
-# 2. Movie System (Il est dans le dossier pages, donc on met "pages/...")
+# 2. Movie System (Cinema)
 movie_page = st.Page(
     "pages/03_Movie_Recommendation_System.py", 
     title="Movie Recommendation System",
+    icon="🎬"
+)
+
+# 3. F1 Elo System (Sport / Data) - LE NOUVEAU PROJET
+f1_page = st.Page(
+    "pages/04_F1_Elo_System.py",
+    title="F1 Elo Rating System",
+    icon="🏎️"
 )
 
 # --- CRÉATION DE LA NAVIGATION (GROUPÉE) ---
 pg = st.navigation(
     {
         "Finance": [portfolio_page],
-        "Other": [movie_page],
+        "Other Projects": [f1_page, movie_page], # J'ai ajouté la page F1 ici
     }
 )
 
